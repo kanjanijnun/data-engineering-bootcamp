b4dcb08dfe5ed3534757e0dcc1c39e26eabc6e5b
Chertam <47439802+kanjanijnun@users.noreply.github.com>
2023-04-23 16:50:41 +0000

    Kanjanijnun Commit

--- 01-introduction-to-data-engineering/workshops/data-engineering-lifecycle/load.sh
+++ 01-introduction-to-data-engineering/workshops/data-engineering-lifecycle/load.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -XPOST -H "Content-type: application/json" -d @dogs.json "https://getpantry.cloud/apiv1/pantry/<YOUR-PANTRY-ID>/basket/randomDogs"
+curl -XPOST -H "Content-type: application/json" -d @dogs.json "https://getpantry.cloud/apiv1/pantry/631eef72-2672-4c56-a79b-66a2a562db2a/basket/randomDogs"
